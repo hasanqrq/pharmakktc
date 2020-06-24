b6865dc627c6c9f681ad39fe0117f91ce79ff8f7
Hasan Qarqur <hasanqrq1992@gmail.com>
2020-06-24 16:09:48 +0300

    added new page and script

---
+++ js/drugs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function drugs() {
+    var items = prompt('please enter how many items do you want');
+
+
+
+    for (var i = 0; i < items; i++) {
+
+        document.write('<img id="block" src="pill.jpg">');
+    }
+}
+drugs();
