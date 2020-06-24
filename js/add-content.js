feccf5530e8b73436f7edaec3260c9867f4919ae
Hasan Qarqur <hasanqrq1992@gmail.com>
2020-06-24 11:31:08 +0300

    add a func

--- js/add-content.js
+++ js/add-content.js
@@ -1,18 +1,23 @@
 'use strict';
 
-var age = prompt('please enter your age');
-var name = prompt('Please enter your name');
-
-var message;
-
-
-
-if (age >= 18 ) {
-  message = 'Welcome to pharmacy website';
-
-} else {
-  message = 'Please consult your parents';
+function question(){
+  var age = prompt('please enter your age');
+  var name = prompt('Please enter your name');
+  
+  var message;
+  
+  
+  
+  if (age >= 18 ) {
+    message = 'Welcome to pharmacy website';
+  
+  } else {
+    message = 'Please consult your parents';
+  }
+  
+  document.write('<h2>' + message +' Mr ' + name +'</h2>');
 }
 
-document.write('<h2>' + message +' Mr ' + name +'</h2>');
+question();
+
 
